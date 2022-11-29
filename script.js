ef5d2d52d99e2a0d0ff872d3246353807df77f33
PRESISPREMACHANDRAN <114502492+PRESISPREMACHANDRAN@users.noreply.github.com>
2022-11-29 17:45:10 +0530

    changes

--- script.js
+++ script.js
@@ -107,41 +107,46 @@ function check(){
 
 
 // table
-fetch("https://jsonplaceholder.typicode.com/todos").then((data)=>{
-return data.json();
-}).then((objectData)=>{
-    let tableData="";
-    objectData.map((values)=>{
-        tableData+= `<tr> 
+let checkedArray = [];
+fetch("https://jsonplaceholder.typicode.com/todos").then((data) => {
+  return data.json();
+}).then((objectData) => {
+  let tableData = "";
+  objectData.forEach((values) => {
+    tableData += `
+        <tr> 
         <td>${values.id}</td>
         <td>${values.title}</td>
-        <td><input type='checkbox'></td>
-        
+        <td>
+          <input 
+              id="Checkbox-${values.id}" 
+              onClick="checkBoxClicked(${values.id})" 
+              type="checkbox">
+              </td>
       </tr>`;
-    });
-    document.getElementById("table_body").innerHTML=tableData;
+  });
+  document.getElementById("table_body").innerHTML = tableData;
+
+  objectData.forEach(data => {
+    let currentComponent = document.getElementById(`Checkbox-${data.id}`)
+    currentComponent.checked = data.completed;
+    currentComponent.disabled = data.completed
+  })
 })
 
-
-// var weather=false;
-const checkbox=new Promise(function(resolve,reject){
-    if(value.completed>=5){
-        const datedetails={
-            
-        }
-        resolve(datedetails)
-
+function checkBoxClicked(id) {
+  checkBoxPromise().then(function (response) {
+    checkedArray.push(id)
+  }).catch(function (error) {
+    alert("You have completed 5 tasks")
+  })
+}
+let checkBoxPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (checkedArray.length < 4) {
+      resolve(true)
+    } else {
+      reject(false)
     }
-    else{
-        reject("select minimum 5 values")
-    } 
-})
-
-dating
-.then(function(date){
-    console.log('date details:',date);
-
-})
-.catch(function(err){
-    console.log(err)
-})
+  })
+}
