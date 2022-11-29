0735d53f97ebb75823dfb6f7dbf7c5cdc7dbbaf1
PRESISPREMACHANDRAN <114502492+PRESISPREMACHANDRAN@users.noreply.github.com>
2022-11-29 16:28:30 +0530

    changes

--- script.js
+++ script.js
@@ -1,58 +1,147 @@
-function validate(callback){
-var fixedusername=/admin/;
-var fixedpassword=/12345/;
-var username=document.getElementById("username");
-var password=document.getElementById("password");
-var usernameerror=document.getElementById("usernameerror");
-var passworderror=document.getElementById("passworderror");
-var x;
-var y;
-if(username.value.trim()==""){
-    usernameerror.innerHTML="Username cannot be empty";
-    usernameerror.style.color="red";
-        x=0;
-} if(password.value.trim()==""){
-    passworderror.innerHTML="Password cannot be empty";
-    passworderror.style.color="red";
-        y=0;
-}
-if(username.value.trim()!=""){
 
-    if(username.value.length<6){
-        if(fixedusername.test(username.value)){
-            x=1;
-            usernameerror.innerHTML="";
-        }else{
+
+
+
+// // ajax and api
+// function ajax(){
+//     var xhttp=new XMLHttpRequest();
+//     xhttp.onreadystatechange=function(){
+//         if(this.readyState==4&&this.status==200){
+//             var response=JSON.parse(this.responseText);
+//             var output="";
+//             for(var i=0;i<response.length;i++){
+//                 // output +="<li>"+response[i].id+response[i].title+"</li>";
+//                 output +=`<tr>
+//                 <td>${response[i]+id}</td>
+//                 <td>${response[i]+title}</td>
+//                 <td>${response[i]+completed}</td>
+//                 </tr>`;
+//             }
+//             document.getElementById('api').innerHTML=output;
+//         }
+//     }
+//     xhttp.open('GET','https://jsonplaceholder.typicode.com/todos',true);
+//     xhttp.send();
+// }
+
+// // promise
+// callback
+function validate(callback){
+    var fixedusername=/admin/;
+    var fixedpassword=/12345/;
+    var username=document.getElementById("username");
+    var password=document.getElementById("password");
+    var usernameerror=document.getElementById("usernameerror");
+    var passworderror=document.getElementById("passworderror");
+
+    var x;
+    var y;
+    // username name and password
+    if(username.value.trim()==""){
+        usernameerror.innerHTML="Username cannot be empty";
+        usernameerror.style.color="red";
+            x=0;
+    } if(password.value.trim()==""){
+        passworderror.innerHTML="Password cannot be empty";
+        passworderror.style.color="red";
+            y=0;
+    }
+    // username
+    if(username.value.trim()!=""){
+
+        if(username.value.length<6){
+            if(fixedusername.test(username.value)){
+                x=1;
+                usernameerror.innerHTML="";
+            }else{
+                x=0;
+                usernameerror.style.color="red";
+            usernameerror.innerHTML="Invalid user name";
+            }
+
+        }
+        else{
             x=0;
             usernameerror.style.color="red";
-        usernameerror.innerHTML="Invalid user name";
+            usernameerror.innerHTML="Invalid user name";
+       }
+    }
+    // password
+    if(password.value.trim()!=""){
+        console.log(password.value.length);
+        if(password.value.length<6){
+            if(fixedpassword.test(password.value)){
+                y=1;
+
+                passworderror.innerHTML="";
+            }else{
+                passworderror.style.color="red";
+                passworderror.innerHTML="Invalid password";
+                y=0;
+            }
+
+
+        }else{
+            y=0;
+            passworderror.style.color="red";
+            passworderror.innerHTML="Invalid password";
         }
+    }
+    // function call
+    if(x==1&&y==1){
+            var z=callback();
+            if(z==true){
+                return true;
+            }
+    }
+    else{
+        return false;
+    }
+}
+function check(){
+    return true;
+}
+
+
+
+
+
+// table
+fetch("https://jsonplaceholder.typicode.com/todos").then((data)=>{
+return data.json();
+}).then((objectData)=>{
+    let tableData="";
+    objectData.map((values)=>{
+        tableData+= `<tr> 
+        <td>${values.id}</td>
+        <td>${values.title}</td>
+        <td><input type='checkbox'></td>
+        
+      </tr>`;
+    });
+    document.getElementById("table_body").innerHTML=tableData;
+})
+
+
+// var weather=false;
+const checkbox=new Promise(function(resolve,reject){
+    if(value.completed>=5){
+        const datedetails={
+            
+        }
+        resolve(datedetails)
 
     }
     else{
-        x=0;
-        usernameerror.style.color="red";
-        usernameerror.innerHTML="Invalid user name";
-   }
-}
-if(password.value.trim()!=""){
-    console.log(password.value.length);
-    if(password.value.length<6){
-        if(fixedpassword.test(password.value)){
-            y=1;
+        reject("select minimum 5 values")
+    } 
+})
 
-            passworderror.innerHTML="";
-        }else{
-            passworderror.style.color="red";
-            passworderror.innerHTML="Invalid password";
-            y=0;
-        }
+dating
+.then(function(date){
+    console.log('date details:',date);
 
-
-    }else{
-        y=0;
-        passworderror.style.color="red";
-        passworderror.innerHTML="Invalid password";
-    }
-}
-}
+})
+.catch(function(err){
+    console.log(err)
+})
